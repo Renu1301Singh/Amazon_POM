7c28a209532357c5fe154bfce777244b535c4502
Renu1301Singh <152848609+Renu1301Singh@users.noreply.github.com>
2024-04-03 12:22:35 +0530

    Update AmazonPageTest.java

--- src/test/java/Amazon/Test/AmazonPageTest.java
+++ src/test/java/Amazon/Test/AmazonPageTest.java
@@ -1,77 +1,81 @@
 package Amazon.Test;
-
+import org.openqa.selenium.chrome.ChromeOptions;
 import Amazon.Pages.AmazonAddToCart;
 import Amazon.Pages.AmazonHomePage;
 import Amazon.Pages.AmazonProceedToBuy;
 import Amazon.Pages.AmazonProductColourPage;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.Assert;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.*;
 
 import java.util.concurrent.TimeUnit;
+
 public class AmazonPageTest {
-   WebDriver driver;
+    WebDriver driver;
     String driverPath = "/usr/bin/chromedriver";
     ChromeOptions options;
     AmazonHomePage amazonHomePage;
-    AmazonProductColourPage amazonProductColourPage; 
-    AmazonAddToCart amazonAddToCart; 
-    AmazonProceedToBuy amazonProceedToBuy; /
+    AmazonProductColourPage amazonProductColourPage;
+    AmazonAddToCart amazonAddToCart;
+    AmazonProceedToBuy amazonProceedToBuy;
 
     @BeforeTest
-    public void setUp(){
+    public void setUp() {
         options = new ChromeOptions();
         options.addArguments("--no-sandbox");
         options.addArguments("--disable-dev-shm-usage");
         options.addArguments("--headless");
-        
+
         System.setProperty("webdriver.chrome.driver", driverPath);
         driver = new ChromeDriver(options);
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         driver.get("https://www.amazon.in/");
-        }
+    }
+
     @Test(priority = 0, description = "This Test validates the Amazon page is launched")
-    void AmazonLaunch(){
-        amazonHomePage=new AmazonHomePage(driver);
+    void AmazonLaunch() {
+        amazonHomePage = new AmazonHomePage(driver);
         amazonHomePage.verifyPageLaunch();
     }
+
     @Test(priority = 1, description = "This Test Boat headphones are searched using Search box")
-    void ProductSearch(){
+    void ProductSearch() {
         amazonHomePage.addsearch("Boat Headphones");
         amazonHomePage.clicksearch();
         amazonHomePage.VerifySearchItem();
     }
-// If the first item appeared on the product list doesn't have colours the test will fail
-    @Test(priority = 2,description = "This test validates First item selected and colour of the item can be changed")
-    public void ProductColour(){
-        amazonProductColourPage=new AmazonProductColourPage(driver);
+
+    // If the first item appeared on the product list doesn't have colours the test will fail
+    @Test(priority = 2, description = "This test validates First item selected and colour of the item can be changed")
+    public void ProductColour() {
+        amazonProductColourPage = new AmazonProductColourPage(driver);
         amazonProductColourPage.SelectProduct();
-        for(String window: driver.getWindowHandles()){
+        for (String window : driver.getWindowHandles()) {
             driver.switchTo().window(window);
         }
         amazonProductColourPage.ChangeColour();
         amazonProductColourPage.IsProductColour();
-
     }
-    @Test(priority = 3,description = "This test validates First item selected is added to cart")
-    public void IsAddedToCart(){
-        amazonAddToCart=new AmazonAddToCart(driver);
+
+    @Test(priority = 3, description = "This test validates First item selected is added to cart")
+    public void IsAddedToCart() {
+        amazonAddToCart = new AmazonAddToCart(driver);
         amazonAddToCart.AddToCartButton();
         amazonAddToCart.navigateToShoppingCart();
         amazonAddToCart.assertShoppingCartPage();
     }
-    @Test(priority = 4,description = "This test validates Proceed to buy Button is clicked")
-    public void IsProceedToBuy(){
-        amazonProceedToBuy=new AmazonProceedToBuy(driver);
+
+    @Test(priority = 4, description = "This test validates Proceed to buy Button is clicked")
+    public void IsProceedToBuy() {
+        amazonProceedToBuy = new AmazonProceedToBuy(driver);
         amazonProceedToBuy.ProceedToBuy();
         amazonProceedToBuy.IsProceedToBuy();
-
     }
-    @AfterTest
 
-    public void tearDown(){
+    @AfterTest
+    public void tearDown() {
         driver.quit();
     }
 }
